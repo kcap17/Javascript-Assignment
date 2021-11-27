32e61e4fedf5dd1f9b9622a77d83daba49d31f5e
kcap17 <abhishek91666@icloud.com>
2021-11-27 17:30:13 +0530

    can_drive

--- assignments/can_drive/can_drive.js
+++ assignments/can_drive/can_drive.js
@@ -1,5 +1,12 @@
 function CanDrive(hasDrivingLiscence, isTired, isSober) {
     // Write you code here
+    if(hasDrivingLiscence===false){
+        return "You cannot drive"
+    }
+    if(hasDrivingLiscence===true && isTired===false && isSober===true){
+        return "You can drive"
+    }
+    return "You shouldn't drive"
 }
 
 module.exports = CanDrive;
